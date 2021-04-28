1ca53c20298c8cd73cce132980f49cc35c52cd8a
James Villanueva <72240605+villanuevajamesfvillanueva@users.noreply.github.com>
2021-04-28 20:42:49 +0800

    Delete exercise1.rb

--- exercise1.rb
+++
@@ -1,9 +0,0 @@
-#item 1
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-arr.each {|e| puts "array value = #{e}"}
-
-#item 2
-h = {a:1, b:2, c:3, d:4}
-puts "\nvalue of  key:b is #{h[:b]}"
-h[:e] = 5
-puts h
