9ddba4ff90882ed1a15d8e3330aa75255d1eaf25
James Villanueva <villanuevajamesfvillanueva>
2021-05-03 20:02:13 +0800

    added animal_batch6.rb file

---
+++ exercise3/animal_batch6.rb
@@ -0,0 +1,14 @@
+class AnimalBatch6
+    def initialize (name, color, age)
+        @name = name
+        @color = color
+        @age = age
+    end
+
+    def walk
+        puts "Hey #{@name}, let's walk!" 
+    end
+end
+
+
+
