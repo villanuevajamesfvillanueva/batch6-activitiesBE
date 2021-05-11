f09b7e7edab761e0d1b7b8e920f2475033371db3
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-05-11 21:37:09 +0800

    added new exercises

--- exercise9/disemvoweledTrolls.rb
+++ exercise9/disemvoweledTrolls.rb
@@ -1,5 +1,7 @@
 def disemvowel(str)
     return str.tr('aeiouAEIOU', '')
+    #seems like tr doesn't accept regex for first arg
+    # return str.tr(/aeiou/i, '')
 end
 
 def disemvowel2(str)
---
+++ personal exercises/codewars/multiplicationTable.rb
@@ -0,0 +1,8 @@
+def multiTable(number)
+    str = ""
+    (1..10).each { |x| str += "#{x} * #{number} = #{x * number}\n" }
+    return str
+end
+
+puts multiTable(5)
+multiTable(1)
