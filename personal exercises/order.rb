979441397c5f2e961dc6af27c2441dd1cccf9d86
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-05-24 19:08:41 +0800

    added test.rb

--- personal exercises/order.rb
+++ personal exercises/order.rb
@@ -1,11 +1,11 @@
 # save
 # instantiate with properties of drink, size, total
 # associated with its parent object :customer
-# belongs to a customer
+# belongs to a customer => setter and getter that allows association to the customer
 
 class Order
     @@all = []          #class variable
-    attr_accessor :drink, :size, :total
+    attr_accessor :drink, :size, :total, :customer
     
     def initialize(drink, size, total)
         @drink = drink
---
+++ personal exercises/test.rb
@@ -0,0 +1,6 @@
+require_relative './order'
+require 'pry'
+
+order1 = Order.new('caramel frappe', 'grande', 3.50)
+binding.pry
+puts ''
