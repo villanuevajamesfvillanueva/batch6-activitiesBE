c431409cdf1bf16bb9bb4c2c26bb2d3ed7041b90
James Villanueva <72240605+villanuevajamesfvillanueva@users.noreply.github.com>
2021-07-25 22:23:08 +0800

    Update and rename multiplication_recustion.rb to multiplication_recursion_decimal.rb

--- personal exercises/multiplication_recursion_decimal.rb
+++ personal exercises/multiplication_recursion_decimal.rb
@@ -1,21 +1,3 @@
-# function that multiplies two positive integers using recursion
-
-def my_func(a, b)
-    return 0 if (a == 0 || b == 0)
-    return a+b-1 if b <= 1
-    a + my_func(a, b-1)
-end
-
-puts my_func(7, 6)
-puts my_func(6, 7)
-puts my_func(12, 10)
-puts my_func(9, 5)
-puts my_func(8, 0)
-puts my_func(0, 8)
-puts my_func(1, 12)
-puts my_func(12, 1)
-puts my_func(0, 0)
-
 # function that multiplies two real numbers using recursion
 
 def pow10(len)
